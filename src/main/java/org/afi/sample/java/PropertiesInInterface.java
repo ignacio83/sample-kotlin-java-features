dfb2a822a3526f0fa2aee70fdfe475d4b9e2c590
Andre de Fontana Ignacio <ignacio83@gmail.com>
2020-08-22 15:54:19 -0300

    PropertiesInInterfaceWithRecords.java moved to PropertiesInInterface.java

--- src/main/java/org/afi/sample/java/PropertiesInInterface.java
+++ src/main/java/org/afi/sample/java/PropertiesInInterface.java
@@ -1,57 +1,96 @@
 package org.afi.sample.java;
 
-// Works, but is not the same thing
 public class PropertiesInInterface {
 
-    public static void main(String... args) {
-        var car = new Car("Fusca");
-        assert car.getName().equals("Fusca");
-        assert car.getWheelsQty() == 4;
+    // Works, but is not the same thing
+    public static class Java11 {
 
-        var motorcycle = new Motorcycle("Bis");
-        assert motorcycle.getName().equals("Bis");
-        assert motorcycle.getWheelsQty() == 2;
+        public static void main(String... args) {
+            var car = new Car("Fusca");
+            assert car.getName().equals("Fusca");
+            assert car.getWheelsQty() == 4;
+
+            var motorcycle = new Motorcycle("Bis");
+            assert motorcycle.getName().equals("Bis");
+            assert motorcycle.getWheelsQty() == 2;
+        }
+
+        public interface Vehicle {
+            String getName();
+
+            Integer getWheelsQty();
+        }
+
+        public static class Car implements Vehicle {
+            private final String name;
+
+            public Car(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public Integer getWheelsQty() {
+                return 4;
+            }
+        }
+
+        public static class Motorcycle implements Vehicle {
+            private final String name;
+
+            public Motorcycle(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public String getName() {
+                return name;
+            }
+
+            @Override
+            public Integer getWheelsQty() {
+                return 2;
+            }
+        }
     }
 
-    public interface Vehicle {
-        String getName();
+    // Works, hm...
+    public static class Java14 {
 
-        Integer getWheelsQty();
-    }
+        public static void main(String... args) {
+            var car = new Car("Fusca");
+            assert car.name().equals("Fusca");
+            assert car.wheelsQty() == 4;
 
-    public static class Car implements Vehicle {
-        private final String name;
-
-        public Car(String name) {
-            this.name = name;
+            var motorcycle = new Motorcycle("Bis");
+            assert motorcycle.name().equals("Bis");
+            assert motorcycle.wheelsQty() == 2;
         }
 
-        @Override
-        public String getName() {
-            return name;
+        public interface Vehicle {
+            String name();
+
+            Integer wheelsQty();
         }
 
-        @Override
-        public Integer getWheelsQty() {
-            return 4;
-        }
-    }
+        public static record Car(String name) implements Vehicle {
 
-    public static class Motorcycle implements Vehicle {
-        private final String name;
-
-        public Motorcycle(String name) {
-            this.name = name;
+            @Override
+            public Integer wheelsQty() {
+                return 4;
+            }
         }
 
-        @Override
-        public String getName() {
-            return name;
-        }
+        public static record Motorcycle(String name) implements Vehicle {
 
-        @Override
-        public Integer getWheelsQty() {
-            return 2;
+            @Override
+            public Integer wheelsQty() {
+                return 2;
+            }
         }
     }
 }
